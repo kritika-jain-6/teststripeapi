e1517cd1d59afda95a67cb9f89549188a3859ff1
Mistry Samarth Dilpesh <samarth.dilpesh@magicsw.com>
2025-05-08 10:36:36 +0530

    fifth

--- index.js
+++ index.js
@@ -1,42 +1,35 @@
-require('dotenv').config();
-const express = require('express');
-const cors = require('cors');
-const Stripe = require('stripe');
+require("dotenv").config();
+const express = require("express");
+const cors = require("cors");
+const Stripe = require("stripe");
 const stripe = Stripe(process.env.STRIPE_SECRET_KEY);
 
 const app = express();
 app.use(cors());
 app.use(express.json());
 
-app.post('/create-subscription', async (req, res) => {
-  console.log(req.body);
-  
+app.post("/create-subscription", async (req, res) => {
   const { email, priceId } = req.body;
 
   try {
-    // Create customer
     const customer = await stripe.customers.create({ email });
 
-    // Create subscription
     const subscription = await stripe.subscriptions.create({
       customer: customer.id,
       items: [{ price: priceId }],
       payment_behavior: 'default_incomplete',
-      expand: ['latest_invoice.payment_intent'],
+      payment_settings: { save_default_payment_method: 'on_subscription' },
+      expand: ['latest_invoice.confirmation_secret'],
     });
-
-    const clientSecret = subscription.latest_invoice.payment_intent.client_secret;
 
     res.send({
-      clientSecret,
-      customerId: customer.id,
       subscriptionId: subscription.id,
+      clientSecret: subscription.latest_invoice.confirmation_secret.client_secret,
     });
+    
   } catch (error) {
     res.status(400).send({ error: { message: error.message } });
   }
 });
 
-  
-
-app.listen(3000, () => console.log('Server running on port 3000'));
+app.listen(3000, () => console.log("Server running on port 3000"));
